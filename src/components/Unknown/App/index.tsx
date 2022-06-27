b963b115b04a97314bd11eaa8231e3cb009793e1
mariiishka <anokhinamasha27@gmail.com>
2022-06-27 20:16:42 +0300

    feat: :sparkles: add ui context provider

--- src/components/Unknown/App/index.tsx
+++ src/components/Unknown/App/index.tsx
@@ -3,6 +3,7 @@ import { getAuth } from 'firebase/auth';
 import React from 'react';
 import { AuthProvider, useFirebaseApp } from 'reactfire';
 import theme from '../../../common/theme';
+import UIContextProvider from '../../UIContext';
 import Root from '../Root';
 
 const App: React.FC = () => {
@@ -13,7 +14,9 @@ const App: React.FC = () => {
     <AuthProvider sdk={auth}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Root />
+        <UIContextProvider>
+          <Root />
+        </UIContextProvider>
       </ThemeProvider>
     </AuthProvider>
   );
